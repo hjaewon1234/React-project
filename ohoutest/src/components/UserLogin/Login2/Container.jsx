06e1ada37b09a2e2873a00e1e4f43d6e4797c694
wg333 <88034910+HGWA333@users.noreply.github.com>
2022-12-14 09:40:07 +0900

    모닝 리베이스2

--- ohoutest/src/components/UserLogin/Login2/Container.jsx
+++ ohoutest/src/components/UserLogin/Login2/Container.jsx
@@ -63,7 +63,7 @@ const Login2Container = () => {
     <>
       <a onClick={accessToken}></a>
       <a onClick={refreshToken}></a>
-      <h3>{user.userName} 님이 로그인했습니다.</h3>
+      <h3>{user.username} 111님이 로그인했습니다.</h3>
       <button onClick={logout}>Logout</button>
       <Login2Components
         setUser={setUser}
