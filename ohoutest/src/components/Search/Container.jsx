2b2b5e4c72fb5cedfad1d9590a4a8bf59d041992
Kimsunju <sophia10047600@gmail.com>
2022-12-19 09:26:12 +0900

    morning rebase

--- ohoutest/src/components/Search/Container.jsx
+++ ohoutest/src/components/Search/Container.jsx
@@ -2,6 +2,8 @@ import SearchComponent from "./Component";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import useDidMountEffect from "../util/useDidMountEffect";
 
 const searchWordAxios = (setState, sword) => {
   axios
@@ -12,11 +14,18 @@ const searchWordAxios = (setState, sword) => {
 };
 
 const SearchContainer = () => {
+  let { sword } = useParams();
   const [itemArr, setItemArr] = useState([]);
   const searchWord = useSelector((state) => state.search.sword);
+
   useEffect(() => {
+    searchWordAxios(setItemArr, sword);
+  }, []);
+
+  useDidMountEffect(() => {
     searchWordAxios(setItemArr, searchWord);
   }, [searchWord]);
+
   useEffect(() => {}, [itemArr]);
   return <SearchComponent itemArr={itemArr} />;
 };
