5d9748dfdd2d350eb091cfac0a3cbfdacd09c21c
hjaewon1234 <hjwl@naver.com>
2022-12-28 17:24:41 +0900

    socket system finish

--- ohoutest/src/components/Chat/Container.jsx
+++ ohoutest/src/components/Chat/Container.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ChatComponent from "./Component";
 import { socket } from "../../service/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
 import { io } from "socket.io-client";
 import { SOCKET_EVENT } from "../../service/socket";
 
@@ -10,26 +11,68 @@ const ChatContainer = () => {
   const [chatAry, setChatAry] = useState([]);
   const [world, setWorld] = useState("whole");
   const userInfo = useSelector((state) => state.userInfo);
+  const noticeInfo = useSelector((state) => state.chatNoticeManager);
+
   const userName = userInfo.userName;
-  const onChatEnter = (e) => {
+  const vipNum = userInfo.userImportance;
+  let uploadFunc;
+
+  const onChatEnter = async (e) => {
     if (e.code == "Enter") {
-      socket.emit("message", { userName: userName, chatValue: chatValue });
+      console.log(world);
+      await axios.post("/api/community/pushChat", {
+        name: userName,
+        text: chatValue,
+        room: world,
+        importance: vipNum,
+      });
+      socket.emit("message", {
+        name: userName,
+        text: chatValue,
+        room: world,
+        importance: vipNum,
+      });
       setChatValue("");
     }
   };
-
+  const chatLog = async (worldName) => {
+    const result = await axios.post("/api/community/getChat", {
+      room: worldName,
+    });
+    console.log(result.data);
+    setChatAry(result.data);
+  };
+  // async 함수로 선언된 함수는 결과가 나올때 await가 없으면 그냥 프로미스로 나온다.
+  // return 값이 나오지 않는다.
+  // chatAry = chatLog();
   // [useruserName, chatValue]
   const onChatting = (e) => {
     setChatValue((state) => state + e.nativeEvent.data);
   };
   useEffect(() => {
-    socket.on("upload", (data) => {
-      setChatAry((state) => [data, ...state]);
-    });
+    uploadFunc = (data) => {
+      console.log(data);
+      if (data.room == world) {
+        console.log(data);
+        setChatAry((state) => [data, ...state]);
+      }
+    };
+    socket.emit("ROOM");
+    socket.on("upload", uploadFunc);
   }, []);
+
   useEffect(() => {
-    socket.emit("ROOM", world);
+    chatLog(world);
     console.log("한번돔", world);
+    socket.off("upload", uploadFunc);
+    uploadFunc = (data) => {
+      console.log(data);
+      if (data.room == world) {
+        console.log(data);
+        setChatAry((state) => [data, ...state]);
+      }
+    };
+    socket.on("upload", uploadFunc);
   }, [world]);
 
   return (
@@ -40,6 +83,8 @@ const ChatContainer = () => {
       onChatting={onChatting}
       userName={userName}
       setWorld={setWorld}
+      noticeInfo={noticeInfo}
+      setChatValue={setChatValue}
     />
   );
 };
